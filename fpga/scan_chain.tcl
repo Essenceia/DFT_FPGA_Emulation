3cbf1bc15986eb0b1b9eed9fe02190c0bc4b9c16
Julia Desmazes - ws <julia.desmazes@gmail.com>
2025-12-10 10:01:12 -0800

    manipulating reg names to match vivado synth results

--- fpga/scan_chain.tcl
+++ fpga/scan_chain.tcl
@@ -29,9 +29,28 @@ proc new_mux { name } {
 	create_cell -reference $mux_ref $name
 } 
 
+proc rework_scan_chain_names { scan_chain } {
+	set newsc {}
+	foreach elem $scan_chain {
+		# clean up escape characters
+		regsub -all {\\} $elem {} elem
+		# replace names that end in '_q' with '_q_reg'
+		regsub {(.*)_q(\[\d+\])*$} $elem {\1_q_reg\2\3\4\5\6} elem
+		# replace names that end in '_o' with '_q_reg'
+		regsub {(.*)_o(\[\d+\])*$} $elem {\1_q_reg\2\3\4\5\6} elem
+
+		# add back escape sequences
+		regsub -all {\[} $elem {\[} elem
+		regsub -all {\]} $elem {\]} elem
+		lappend newsc $elem
+	}
+	return $newsc
+}
+
 proc check_net_equivalence { scan_chain } {
 	set_msg_config -id "ScanChain 1" -limit -1 -new_severity WARNING	
-	set_msg_config -id "ScanChain 2" -limit -1 -new_severity INFO	
+	set_msg_config -id "ScanChain 2" -limit -1 -new_severity INFO
+	puts "Hello"	
 	foreach elem $scan_chain {
 		set pattern {.*}
 		set cell [get_cells -hierarchical -regexp $pattern$elem$pattern ]
