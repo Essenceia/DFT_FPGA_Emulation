6f8715b2af36704310fb89ffc5ac2bbf9d0b8d06
Julia Desmazes - ws <julia.desmazes@gmail.com>
2025-12-12 13:00:18 -0800

    wrapping it together

---
+++ fpga/scan_chain.tcl
@@ -0,0 +1,14 @@
+# args
+set asic_dff_csv_path {../dft/results/translation-tcl.csv}
+set remap_csv_path {scan_chain_remapping.csv}}
+set mux_ref [get_cells -regexp {.*m_smux.*}
+set sci_pin [get_pins -regexp  {.*m_jtag_tap.scan_in_o.*}]
+set sco_pin [get_pins -regexp  {.*m_jtag_tap.scan_out_i.*}]
+set sce_pin [get_pins -regexp  {.*m_jtag_tap.scan_enable_o.*}]
+ 
+
+source sc_remap_utils.tcl
+source sc_netlist_utils.tcl
+
+set ff_dict [read_scan_chain $asic_dff_csv_path $remap_csv_path]
+insert_scan_chain $ff_dict $mux_ref $sci_pin $sco_pin $sce_pin]
